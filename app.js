45676460986387f81439f8590830714ef37428c6
Your Name <you@example.com>
2026-04-14 08:53:14 -0400

    fix directories

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs")
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.set("views", path.join(__dirname, "views"))
 
 app.get('/', (req, res) => {
